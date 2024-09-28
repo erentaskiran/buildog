104a41cc3d78e2f3e4a6108a1d4e91f2195834ab
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2024-09-28 16:04:21 +0300

    Refactor adding user to organization flow

--- apps/api/internal/api/organization_user.go
+++ apps/api/internal/api/organization_user.go
@@ -40,58 +40,60 @@ func (a *api) addUserToOrganization(w http.ResponseWriter, r *http.Request) {
 
 	if role == "admin" || role == "owner" {
 		user, err := a.userRepo.GetUserWithEmail(payload.Email)
-		if err.Error() == "user not found" {
-			err = firebase.InitFirebase()
-			if err != nil {
-				utils.JSONError(w, http.StatusInternalServerError, err.Error())
-				return
-			}
+		if err != nil {
+			if err.Error() == "user not found" {
+				err := firebase.InitFirebase()
+				if err != nil {
+					utils.JSONError(w, http.StatusInternalServerError, err.Error())
+					return
+				}
 
-			password, err := utils.GeneratePassword(12)
-			if err != nil {
-				utils.JSONError(w, http.StatusInternalServerError, err.Error())
-				return
-			}
+				password, err := utils.GeneratePassword(12)
+				if err != nil {
+					utils.JSONError(w, http.StatusInternalServerError, err.Error())
+					return
+				}
 
-			err = firebase.CreateUserWithEmail(payload.Email, password)
-			if err != nil {
-				utils.JSONError(w, http.StatusInternalServerError, err.Error())
-				return
-			}
+				err = firebase.CreateUserWithEmail(payload.Email, password)
+				if err != nil {
+					utils.JSONError(w, http.StatusInternalServerError, err.Error())
+					return
+				}
 
-			firebaseUser, err := firebase.GetUserByEmail(payload.Email)
-			if err != nil {
-				utils.JSONError(w, http.StatusInternalServerError, err.Error())
-				return
-			}
+				firebaseUser, err := firebase.GetUserByEmail(payload.Email)
+				if err != nil {
+					utils.JSONError(w, http.StatusInternalServerError, err.Error())
+					return
+				}
 
-			newUser := &models.User{
-				Id:        firebaseUser.UID,
-				FirstName: "Unknown",
-				LastName:  "Unknown",
-				Email:     payload.Email,
-				CreatedAt: time.Now(),
-				UpdatedAt: time.Now(),
-			}
+				newUser := &models.User{
+					Id:        firebaseUser.UID,
+					FirstName: "Unknown",
+					LastName:  "Unknown",
+					Email:     payload.Email,
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				}
 
-			_, err = a.userRepo.CreateUser(newUser)
-			if err != nil {
-				utils.JSONError(w, http.StatusInternalServerError, err.Error())
-				return
-			}
+				_, err = a.userRepo.CreateUser(newUser)
+				if err != nil {
+					utils.JSONError(w, http.StatusInternalServerError, err.Error())
+					return
+				}
 
-			user, err = a.userRepo.GetUserWithEmail(payload.Email)
-			if err != nil {
+				user, err = a.userRepo.GetUserWithEmail(payload.Email)
+				if err != nil {
+					log.Printf("Error get user: %v", err)
+					utils.JSONError(w, http.StatusInternalServerError, "No permission")
+					return
+				}
+
+				utils.SendEmail(payload.Email, password, &a.cfg)
+			} else {
 				log.Printf("Error get user: %v", err)
 				utils.JSONError(w, http.StatusInternalServerError, "No permission")
 				return
 			}
-
-			utils.SendEmail(payload.Email, password, &a.cfg)
-		} else if err != nil && err.Error() != "user not found" {
-			log.Printf("Error get user: %v", err)
-			utils.JSONError(w, http.StatusInternalServerError, "No permission")
-			return
 		}
 
 		organization_user := &models.OrganizationUserCreated{
